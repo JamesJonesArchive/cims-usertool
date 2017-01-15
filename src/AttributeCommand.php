47f1cc54727eb6a11a60cfe216066425817c4c23
James Jones <jamjon3@gmail.com>
2017-01-14 21:25:59 -0500

    Stubbing the attribute stuff

--- src/AttributeCommand.php
+++ src/AttributeCommand.php
@@ -11,7 +11,8 @@ namespace USF\Idm\usertool;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 /**
  * Description of AttributeCommand
  *
@@ -28,10 +29,48 @@ class AttributeCommand extends Command {
 
         // the full command description shown when running the command with
         // the "--help" option
-        ->setHelp("This command allows you to post attribute changes to the AD change queue");
+        ->setHelp("This command allows you to post attribute changes to the AD change queue")
+        // configure an argument
+        ->addArgument('username', InputArgument::REQUIRED, 'The username for modification')
+        ->addOption(
+            'key',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'The Attribute Key',
+            1
+        )
+        ->addOption(
+            'value',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'value',
+            1
+        );
     }
     
-    protected function execute(InputInterface $input, OutputInterface $output) {}
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        
+        
+        
+        // outputs multiple lines to the console (adding "\n" at the end of each line)
+        $output->writeln([
+            'User Creator',
+            '============',
+            '',
+        ]);
+
+        // outputs a message followed by a "\n"
+        $output->writeln('Whoa!');
+
+        // outputs a message without adding a "\n" at the end of the line
+        $output->write('You are about to ');
+        $output->write('create a user.');        
+        
+        $output->writeln('Key: '.$input->getArgument('key'));
+        
+        
+        
+    }
     
             
     //put your code here
