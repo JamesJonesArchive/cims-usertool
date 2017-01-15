cfe07e96a2f9f0a0f35c70981a2cdb8b6ad430a0
James Jones <jamjon3@gmail.com>
2017-01-14 22:38:35 -0500

    Stubbing the attribute stuff

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ vendor/bin/box build
 echo "**************************************************************************"
 echo "* Note: php.ini must have phar.readonly = Off set to build the phar file *"
 echo "**************************************************************************"
+rm -Rf bin/*.rpm
+echo "**************************************************************************"
+echo "* Building the bin RPM file                                      *"
+echo "**************************************************************************"
+fpm -s dir -t rpm -n 'cimsusertool' -v '1.0.0' --iteration '1' --description 'USF Command-line tool for AD requests' --vendor 'University of South Florida' -p 'bin' 'bin/cimsusertool.phar'='/usr/local/bin/cimsusertool' 
