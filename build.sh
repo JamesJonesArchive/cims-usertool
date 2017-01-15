57044fa0d8b91534d32d11b78a6c892dd389df5d
James Jones <jamjon3@gmail.com>
2017-01-15 11:54:39 -0500

    Cleaning up

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "**************************************************************************"
-echo "* Updating composer for the armpit package                               *"
+echo "* Updating composer for the usertool package                             *"
 echo "**************************************************************************"
 composer update
 echo "**************************************************************************"
-echo "* Building the bin/armpit.phar file                                      *"
+echo "* Building the bin/cimsusertool.phar file                                *"
 echo "**************************************************************************"
 vendor/bin/box build
 echo "**************************************************************************"
@@ -12,6 +12,6 @@ echo "* Note: php.ini must have phar.readonly = Off set to build the phar file *
 echo "**************************************************************************"
 // rm -Rf bin/*.rpm
 echo "**************************************************************************"
-echo "* Building the bin RPM file                                      *"
+echo "* Building the bin cimusertool RPM file                                  *"
 echo "**************************************************************************"
 fpm --force -s dir -t rpm -n 'cimsusertool' -v '1.0.0' --iteration '1' --description 'USF Command-line tool for AD requests' --vendor 'University of South Florida' --config-files '/usr/local/etc/casvoterimport/settings.yml' -p 'bin' 'bin/cimsusertool.phar'='/usr/local/bin/cimsusertool' 'config/settings.yml'='/usr/local/etc/casvoterimport/settings.yml'
